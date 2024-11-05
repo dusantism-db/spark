5bf04f0440ffefe194c8d325d12a17157b786580
Dušan Tišma <dusan.tisma@databricks.com>
2024-11-05 12:43:43 +0100

    adding tests

--- sql/core/src/main/scala/org/apache/spark/sql/scripting/SqlScriptingExecutionNode.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/scripting/SqlScriptingExecutionNode.scala
@@ -762,7 +762,7 @@ class ForStatementExec(
       defaultExpression,
       replace = true
     )
-    new SingleStatementExec(declareVariable, Origin(), false)
+    new SingleStatementExec(declareVariable, Origin(), isInternal = true)
   }
 
   private def createSetVarExec(variable: Expression): SingleStatementExec = {
@@ -772,7 +772,7 @@ class ForStatementExec(
     )
     val setIdentifierToCurrentRow =
       SetVariable(Seq(UnresolvedAttribute(identifier.get)), projectNamedStruct)
-    new SingleStatementExec(setIdentifierToCurrentRow, Origin(), false)
+    new SingleStatementExec(setIdentifierToCurrentRow, Origin(), isInternal = true)
   }
 
   override def getTreeIterator: Iterator[CompoundStatementExec] = treeIterator
